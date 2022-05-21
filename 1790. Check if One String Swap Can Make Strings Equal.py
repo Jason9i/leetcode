d57f626379b641d16b746b0e5a87816b20eb407b
Jason9i <48929940+Jason9i@users.noreply.github.com>
2022-05-21 16:06:31 -0700

    Update 1790. Check if One String Swap Can Make Strings Equal.py

--- 1790. Check if One String Swap Can Make Strings Equal.py
+++ 1790. Check if One String Swap Can Make Strings Equal.py
@@ -11,3 +11,22 @@ class Solution:
             return False
         return True
 
+
+class Solution:
+    def areAlmostEqual(self, s1: str, s2: str) -> bool:
+        if s1 == s2:
+            return True
+        cnt = 0
+        diff1 = []
+        diff2 = []
+        for idx in range(len(s1)):
+            if s1[idx] != s2[idx]:
+                cnt += 1
+                diff1.append(s1[idx])
+                diff2.append(s2[idx])
+
+        if cnt > 2:
+            return False
+        elif sorted(diff1) != sorted(diff2):
+            return False
+        return True
